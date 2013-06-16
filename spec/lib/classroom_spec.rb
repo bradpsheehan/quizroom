386a4d42e2c8382921fe5d7cf155c2746bd850b2
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-16 13:31:00 -0600

    invalid params classroom spec

--- spec/lib/classroom_spec.rb
+++ spec/lib/classroom_spec.rb
@@ -6,17 +6,23 @@ describe Classroom do
     context "valid input" do
       it "creates a Classroom" do
         VCR.use_cassette('classroom create') do
-          classroom = Classroom.create(1, "bro blair 101")
+          classroom = Classroom.create(1, "bro blair 102")
 
-          expect(classroom.name).to eq("bro blair 101")
+          expect(classroom.name).to eq("bro blair 102")
           expect(classroom.teacher_id).to eq(1)
           expect(classroom.id).to_not be_nil
         end
       end
     end
 
-    context "invalid input" do
-      it "returns nil"
+    context "no name inluded" do
+      it "returns nil" do
+        VCR.use_cassette('classroom create with no name') do
+          classroom = Classroom.create(1, "")
+
+          expect(classroom).to eq(nil)
+        end
+      end
     end
   end
 
