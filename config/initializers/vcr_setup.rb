8c5d1b2e8c7c56fa026bed4990d27c501d92ecd5
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-16 18:12:13 -0600

    vcr

--- config/initializers/vcr_setup.rb
+++ config/initializers/vcr_setup.rb
@@ -4,5 +4,6 @@ if Rails.env.test?
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
     c.hook_into :fakeweb
+    c.allow_http_connections_when_no_cassette = true
   end
 end
--- spec/support/utilities.rb
+++
@@ -1,5 +0,0 @@
-def current_classroom
-  1
-  # session[:user]
-end
-
