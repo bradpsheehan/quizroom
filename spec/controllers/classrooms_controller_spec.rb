d79dd1cb053564c3dac5028f3285378ac2d5f7d2
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-16 18:11:33 -0600

    pending GET #show

--- spec/controllers/classrooms_controller_spec.rb
+++ spec/controllers/classrooms_controller_spec.rb
@@ -7,13 +7,15 @@ describe ClassroomsController do
     context "given valid parameters" do
 
       it "sends JSON to classroom service" do
-        classroom = Classroom.create(1, "English 101")
-        controller.stub_chain(:current_user, :id).and_return(1)
-        Classroom.stub(:create).with(1, "English 101").and_return(classroom)
 
-        post :create, class_name: "English 101"
-        expect(assigns(:classroom)).to eq classroom
+        VCR.use_cassette("create a classroom") do
+          controller.stub_chain(:current_user, :id).and_return(14)
 
+          post :create, class_name: "Blair is a bro 101"
+          classroom = assigns(:classroom)
+          expect(classroom.name ).to eq "Blair is a bro 101"
+          expect(classroom.teacher_id ).to eq 14
+        end
       end
     end
 
@@ -21,11 +23,29 @@ describe ClassroomsController do
 
       it "sends JSON without a classroom name to classroom service" do
         controller.stub_chain(:current_user, :id).and_return(1)
-        Classroom.stub(:create).with(1, "")
+
 
         post :create, class_name: ""
         expect(assigns(:classroom)).to eq nil
 
+      end
+    end
+  end
+
+  describe "#show" do
+
+    context "given a classroom id" do
+
+      xit "assigns all messages for that classroom to @messages" do
+        classroom_object = OpenStruct.new(
+                                         :name => "blair's kale",
+                                         :teacher_id => 1,
+                                         :id => 2
+                                        )
+        # message = Message.create(text: "brooooooo dude")
+        classroom = Classroom.stub(:create).and_return(classroom_object)
+
+        get :show
       end
     end
   end
