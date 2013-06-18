a2ec01a2f9461592f21340fdeaa3984d22be573c
erindrummond <e.b.drummond@gmail.com>
2013-06-17 18:55:32 -0600

    passing feature spec for adding students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,11 +7,16 @@ class StudentsController < ApplicationController
   def create
     @students = User.find_or_create_students(params[:students])
     if @students
-      redirect_to new_classroom_student_path(params[:classroom_id]),
+      redirect_to classroom_students_path(params[:classroom_id]),
       notice: "Students successfully created."
     else
       redirect_to new_classroom_student_path(params[:classroom_id]),
       notice: "Something went wrong while creating students"
     end
+  end
+
+  def index
+    @classroom = Classroom.find(current_user.id, params[:classroom_id])
+    @students = User.all #TODO make this only students associated with the classroom
   end
 end
