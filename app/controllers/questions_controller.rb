a48ca8f5ae1f6fe0b1d5e302feee604aa091b90f
Bradley Sheehan <bradpsheehan@gmail.com>
2013-07-02 17:44:57 -0600

    Add multiple questions and answers to quiz

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,6 @@ class QuestionsController < ApplicationController
   def create
 
     @question = Question.create!(quiz_id: params[:quiz_id], question: params[:question][:question])
-    answer = @question.answers.create(question_id: @question.id, answer: params[:answer])
     respond_with @question, location: quiz_question_path(@question.quiz_id, @question)
   end
 
