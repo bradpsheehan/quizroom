fe06e249ce809d8a2a718900095adf873043cb31
Danny Garcia <dannygarcia.me@gmail.com>
2013-07-03 12:09:22 -0600

    faye.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def broadcast(channel, &block)
 
     message = {:channel => channel, :data => capture(&block)}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://www.dannygarcia.me:9292/faye.js")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
